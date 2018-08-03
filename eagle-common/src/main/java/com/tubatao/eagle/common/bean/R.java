3311fe5c5f55bf8e302d941ac011c8da9b150148
liuzhiheng <liuzhiheng@tubatao.com>
2018-08-03 17:21:27 +0800

    feat: add response message body

---
+++ eagle-common/src/main/java/com/tubatao/eagle/common/bean/R.java
@@ -0,0 +1,75 @@
+/**
+ * Copyright(c) tubatao Technology Co.,Ltd.
+ * All Rights Reserved.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * <p>
+ * project: eagle-framework
+ * <p>
+ * Revision History:
+ * Date         Version     Name                Description
+ * 8/2/2018  1.0         liuzhiheng         Creation File
+ */
+package com.tubatao.eagle.common.bean;
+
+import lombok.Data;
+import java.io.Serializable;
+
+/**
+ * Description:
+ * Response message body
+ *
+ * @author liuzhiheng
+ * @date 8/2/2018 11:21 AM
+ */
+@Data
+public class R<T> implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    public static final int NO_LOGIN = -1;
+
+    public static final int SUCCESS = 0;
+
+    public static final int FAIL = 1;
+
+    public static final int NO_PERMISSION = 2;
+
+    private String msg = "success";
+
+    private int code = SUCCESS;
+
+    private T data;
+
+    public R() {
+        super();
+    }
+
+    public R(T data) {
+        super();
+        this.data = data;
+    }
+
+    public R(T data, String msg) {
+        super();
+        this.data = data;
+        this.msg = msg;
+    }
+
+    public R(Throwable e) {
+        super();
+        this.msg = e.getMessage();
+        this.code = FAIL;
+    }
+
+}
