ee775f1928ebbfafd086df946d14198283bce95e
liuzhiheng <liuzhiheng@tubatao.com>
2018-08-08 18:47:16 +0800

    feat: add aop for controller

---
+++ eagle-common/src/main/java/com/tubatao/eagle/core/aop/ControllerAop.java
@@ -0,0 +1,107 @@
+/**
+ * Copyright(c) tubatao Technology Co.,Ltd.
+ * All Rights Reserved.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * <p>
+ * project: eagle-framework
+ * <p>
+ * Revision History:
+ * Date         Version     Name                Description
+ * 7/31/2018  1.0         liuzhiheng         Creation File
+ */
+package com.tubatao.eagle.core.aop;
+
+import com.tubatao.eagle.common.constant.SecurityConstants;
+import com.tubatao.eagle.util.UserUtils;
+import com.xiaoleilu.hutool.util.StrUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+
+/**
+ * Description:
+ *
+ * @author liuzhiheng
+ * @date 7/31/2018 3:16 PM
+ */
+@Slf4j
+@Aspect
+@Component
+public class ControllerAop {
+    @Pointcut("execution(public com.tubatao.eagle.core.aop.ControllerAop *(..))")
+    public void pointCutR() {
+    }
+
+    /**
+     * 拦截器具体实现
+     *
+     * @param pjp 切点 所有返回对象R
+     * @return R  结果包装
+     */
+    @Around("pointCutR()")
+    public Object methodRHandler(ProceedingJoinPoint pjp) throws Throwable {
+        return methodHandler(pjp);
+    }
+
+    @Pointcut("execution(public com.baomidou.mybatisplus.plugins.Page *(..))")
+    public void pointCutPage() {
+    }
+
+    /**
+     * 拦截器具体实现
+     *
+     * @param pjp 切点 所有返回对象Page
+     * @return R  结果包装
+     */
+    @Around("pointCutPage()")
+    public Object methodPageHandler(ProceedingJoinPoint pjp) throws Throwable {
+        return methodHandler(pjp);
+    }
+
+    private Object methodHandler(ProceedingJoinPoint pjp) throws Throwable {
+        long startTime = System.currentTimeMillis();
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+
+        String username = request.getHeader(SecurityConstants.USER_HEADER);
+        if (StrUtil.isNotBlank(username)) {
+            log.info("Controller AOP get username:{}", username);
+            UserUtils.setUser(username);
+        }
+
+        log.info("URL : " + request.getRequestURL().toString());
+        log.info("HTTP_METHOD : " + request.getMethod());
+        log.info("IP : " + request.getRemoteAddr());
+        log.info("CLASS_METHOD : " + pjp.getSignature().getDeclaringTypeName() + "." + pjp.getSignature().getName());
+        log.info("ARGS : " + Arrays.toString(pjp.getArgs()));
+
+        Object result;
+
+        result = pjp.proceed();
+        log.info(pjp.getSignature() + "use time:" + (System.currentTimeMillis() - startTime));
+
+        if (StrUtil.isNotEmpty(username)) {
+            UserUtils.clearAllUserInfo();
+        }
+
+        return result;
+    }
+}
