045db3234d7f24f9cac4af8d8f49fbe065a3792c
liuzhiheng <liuzhiheng@tubatao.com>
2018-08-08 18:42:05 +0800

    feat: add data scope

---
+++ eagle-common/src/main/java/com/tubatao/eagle/common/interceptor/DataScope.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright(c) tubatao Technology Co.,Ltd.
+ * All Rights Reserved.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * <p>
+ * project: eagle-framework
+ * <p>
+ * Revision History:
+ * Date         Version     Name                Description
+ * 8/1/2018  1.0         liuzhiheng         Creation File
+ */
+package com.tubatao.eagle.common.interceptor;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Description:
+ * 数据权限、参考guns实现
+ *
+ * @author liuzhiheng
+ * @date 8/1/2018 1:56 PM
+ */
+
+@Data
+public class DataScope extends HashMap {
+    /**
+     * 限制范围的字段名称
+     */
+    private String scopeName = "dept_id";
+
+    /**
+     * 具体的数据范围
+     */
+    private List<Integer> deptIds;
+
+    /**
+     * 是否只查询本部门
+     */
+    private Boolean isOnly = false;
+}
