6fbd1e474328df6ca3147fda4215004c82c92c28
liuzhiheng <liuzhiheng@tubatao.com>
2018-08-22 15:48:33 +0800

    feat: add swaggerConfig

---
+++ eagle-common/src/main/java/com/tubatao/eagle/core/config/SwaggerConfig.java
@@ -0,0 +1,82 @@
+/**
+ * Copyright(c) tubatao Technology Co.,Ltd.
+ * All Rights Reserved.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * <p>
+ * project: eagle-framework
+ * <p>
+ * Revision History:
+ * Date         Version     Name                Description
+ * 8/1/2018  1.0         liuzhiheng         Creation File
+ */
+package com.tubatao.eagle.core.config;
+
+import io.swagger.annotations.ApiOperation;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.Parameter;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Description:
+ *
+ * @author liuzhiheng
+ * @date 8/1/2018 12:43 PM
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket createRestApi() {
+        ParameterBuilder tokenBuilder = new ParameterBuilder();
+        List<Parameter> parameterList = new ArrayList<>();
+        tokenBuilder.name("Authorization")
+                .defaultValue("去其他请求中获取heard中token参数")
+                .description("令牌")
+                .modelRef(new ModelRef("string"))
+                .parameterType("header")
+                .required(true).build();
+        parameterList.add(tokenBuilder.build());
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                .paths(PathSelectors.any())
+                .build()
+                .globalOperationParameters(parameterList);
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Eagle Swagger API ")
+                .description("https://github.com/tubatao/eagle-framework/wiki")
+                .termsOfServiceUrl("https://github.com/tubatao/eagle-framework")
+                .contact(new Contact("liuzhiheng","https://github.com/tubatao/eagle-framework","liuzhiheng@tubatao.com"))
+                .version("1.0")
+                .build();
+    }
+
+}
