2f8158d44743c424c3af174c4c62d45062718630
liuzhiheng <liuzhiheng@tubatao.com>
2018-08-22 17:08:15 +0800

    feat: add mobileAuthenticationFilter

---
+++ eagle-authorization-server/src/main/java/com/tubatao/eagle/authorization/mobile/MobileAuthenticationFilter.java
@@ -0,0 +1,129 @@
+/**
+ * Copyright(c) tubatao Technology Co.,Ltd.
+ * All Rights Reserved.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * <p>
+ * project: eagle-authorization-server
+ * <p>
+ * Revision History:
+ * Date         Version     Name                Description
+ * 7/11/18  1.0         liuzhiheng         Creation File
+ */
+package com.tubatao.eagle.authorization.mobile;
+
+import com.tubatao.eagle.authorization.constants.SecurityConstants;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.util.Assert;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Description:
+ * Mobile Authentication Filter
+ * An {@link org.springframework.security.core.Authentication} implementation that is
+ * designed for simple presentation of a mobile and smscode.
+ * <p>
+ * The <code>principal</code> and <code>credentials</code> should be set with an
+ * <code>Object</code> that provides the respective property via its
+ * <code>Object.toString()</code> method. The simplest such <code>Object</code> to use is
+ * <code>String</code>.
+ *
+ * @author liuzhiheng
+ * @date 7/11/18 3:54 PM
+ */
+public class MobileAuthenticationFilter extends AbstractAuthenticationProcessingFilter{
+    public static final String SPRING_SECURITY_FORM_MOBILE_KEY = "mobile";
+    public static final String SPRING_SECURITY_FORM_CODE_KEY = "code";
+
+    private String mobileParameter = SPRING_SECURITY_FORM_MOBILE_KEY;
+    private String codeParameter = SPRING_SECURITY_FORM_CODE_KEY;
+    private boolean postOnly = true;
+
+    public MobileAuthenticationFilter() {
+        super(new AntPathRequestMatcher(SecurityConstants.MOBILE_TOKEN_URL, "POST"));
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request,
+                                                HttpServletResponse response) throws AuthenticationException {
+        if (postOnly && !request.getMethod().equals(HttpMethod.POST.name())) {
+            throw new AuthenticationServiceException(
+                    "Authentication method not supported: " + request.getMethod());
+        }
+
+        String mobile = obtainMobile(request);
+        String code = obtainCode(request);
+
+        if (mobile == null) {
+            mobile = "";
+        }
+
+        if (code==null){
+            code="";
+        }
+
+        mobile = mobile.trim();
+
+        MobileAuthenticationToken mobileAuthenticationToken = new MobileAuthenticationToken(mobile,code);
+
+        setDetails(request, mobileAuthenticationToken);
+
+        return this.getAuthenticationManager().authenticate(mobileAuthenticationToken);
+    }
+
+    protected String obtainMobile(HttpServletRequest request) {
+        return request.getParameter(this.mobileParameter);
+    }
+
+    protected String obtainCode(HttpServletRequest request){
+        return request.getParameter(this.codeParameter);
+    }
+
+    protected void setDetails(HttpServletRequest request,
+                              MobileAuthenticationToken authRequest) {
+        authRequest.setDetails(authenticationDetailsSource.buildDetails(request));
+    }
+
+    public void setPostOnly(boolean postOnly) {
+        this.postOnly = postOnly;
+    }
+
+    public final String getMobileParameter() {
+        return mobileParameter;
+    }
+
+    public void setMobileParameter(String mobileParameter) {
+        Assert.hasText(mobileParameter, "Mobile parameter must not be empty or null");
+        this.mobileParameter = mobileParameter;
+    }
+
+    public final String getCodeParameter() {
+        return codeParameter;
+    }
+
+    public void setCodeParameter(String codeParameter) {
+        Assert.hasText(codeParameter, "SmsCode parameter must not be empty or null");
+        this.codeParameter = codeParameter;
+    }
+
+    public boolean isPostOnly() {
+        return postOnly;
+    }
+}
