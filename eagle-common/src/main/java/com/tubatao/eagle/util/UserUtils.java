b6f207e17a8eb615845fb244f8ac6180ff63d782
liuzhiheng <liuzhiheng@tubatao.com>
2018-08-22 15:09:51 +0800

    feat: add  userUtils

---
+++ eagle-common/src/main/java/com/tubatao/eagle/util/UserUtils.java
@@ -0,0 +1,111 @@
+/**
+ * Copyright(c) tubatao Technology Co.,Ltd.
+ * All Rights Reserved.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * <p>
+ * project: eagle-framework
+ * <p>
+ * Revision History:
+ * Date         Version     Name                Description
+ * 8/1/2018  1.0         liuzhiheng         Creation File
+ */
+package com.tubatao.eagle.util;
+
+import com.alibaba.ttl.TransmittableThreadLocal;
+import com.tubatao.eagle.common.constant.CommonConstant;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.MDC;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Base64;
+import java.util.List;
+
+/**
+ * Description:
+ * 用户相关工具类
+ *
+ * @author liuzhiheng
+ * @date 8/1/2018 2:36 PM
+ */
+@Slf4j
+public class UserUtils {
+    private static final ThreadLocal<String> THREAD_LOCAL_USER = new TransmittableThreadLocal<>();
+    private static final String KEY_USER = "user";
+
+    /**
+     * 根据请求heard中的token获取用户角色
+     *
+     * @param httpServletRequest request
+     * @return 角色名
+     */
+    public static List<String> getRole(HttpServletRequest httpServletRequest) {
+        String token = getToken(httpServletRequest);
+        String key = Base64.getEncoder().encodeToString(CommonConstant.SIGN_KEY.getBytes());
+        Claims claims = Jwts.parser().setSigningKey(key).parseClaimsJws(token).getBody();
+        List<String> roleNames = (List<String>) claims.get("authorities");
+        return roleNames;
+    }
+
+    /**
+     * 根据header中的token获取用户ID
+     *
+     * @param httpServletRequest
+     * @return 用户ID
+     */
+    public static Integer getUserId(HttpServletRequest httpServletRequest) {
+        String token = getToken(httpServletRequest);
+        String key = Base64.getEncoder().encodeToString(CommonConstant.SIGN_KEY.getBytes());
+        Claims claims = Jwts.parser().setSigningKey(key).parseClaimsJws(token).getBody();
+        Integer userId = (Integer) claims.get("userId");
+        return userId;
+    }
+
+    /**
+     * 获取请求中token
+     *
+     * @param httpServletRequest request
+     * @return token
+     */
+    public static String getToken(HttpServletRequest httpServletRequest) {
+        String authorization = httpServletRequest.getHeader(CommonConstant.REQ_HEADER);
+        return StringUtils.substringAfter(authorization, CommonConstant.TOKEN_SPLIT);
+    }
+
+    /**
+     * 设置用户信息
+     *
+     * @param username 用户名
+     */
+    public static void setUser(String username) {
+        THREAD_LOCAL_USER.set(username);
+        MDC.put(KEY_USER, username);
+    }
+
+    /**
+     * 从threadlocal 获取用户名
+     *
+     * @return 用户名
+     */
+
+    public static String getUser() {
+        return THREAD_LOCAL_USER.get();
+    }
+
+    public static void clearAllUserInfo() {
+        THREAD_LOCAL_USER.remove();
+        MDC.remove(KEY_USER);
+    }
+}
