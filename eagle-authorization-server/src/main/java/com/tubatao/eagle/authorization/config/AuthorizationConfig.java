2a4718270716be41d1442859b5ac45ac1af719d1
liuzhiheng <liuzhiheng@tubatao.com>
2018-08-22 17:12:01 +0800

    feat: add authorizationConfig

---
+++ eagle-authorization-server/src/main/java/com/tubatao/eagle/authorization/config/AuthorizationConfig.java
@@ -0,0 +1,94 @@
+/**
+ * Copyright(c) tubatao Technology Co.,Ltd.
+ * All Rights Reserved.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * <p>
+ * project: eagle-authorization-server
+ * <p>
+ * Revision History:
+ * Date         Version     Name                Description
+ * 7/12/18  1.0         liuzhiheng         Creation File
+ */
+package com.tubatao.eagle.authorization.config;
+
+import com.tubatao.eagle.authorization.constants.SecurityConstants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
+import org.springframework.security.oauth2.provider.token.*;
+import javax.sql.DataSource;
+
+/**
+ * Description:
+ * 认证服务器逻辑实现
+ *
+ * @author liuzhiheng
+ * @date 7/12/18 9:12 AM
+ */
+@Configuration
+@Order(Integer.MIN_VALUE)
+@EnableAuthorizationServer
+public class AuthorizationConfig extends AuthorizationServerConfigurerAdapter {
+    @Autowired
+    private DataSource dataSource;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private UserDetailsService userDetailsService;
+    @Autowired
+    private TokenStore redisTokenStore;
+    @Autowired
+    private AuthorizationServerTokenServices defaultTokenServices;
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        JdbcClientDetailsService clientDetailsService = new JdbcClientDetailsService(dataSource);
+        clientDetailsService.setSelectClientDetailsSql(SecurityConstants.DEFAULT_SELECT_STATEMENT);
+        clientDetailsService.setFindClientDetailsSql(SecurityConstants.DEFAULT_FIND_STATEMENT);
+        clients.withClientDetails(clientDetailsService);
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+        endpoints
+                .tokenStore(redisTokenStore)
+                .tokenServices(defaultTokenServices)
+                .authenticationManager(authenticationManager)
+                .userDetailsService(userDetailsService);
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security
+                .allowFormAuthenticationForClients()
+                .tokenKeyAccess("isAuthenticated()")
+                .checkTokenAccess("permitAll()");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
