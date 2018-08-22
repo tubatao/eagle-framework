d65cee4de58d4f3aec3c988a4d227cde9315a6a4
liuzhiheng <liuzhiheng@tubatao.com>
2018-08-22 17:12:30 +0800

    feat: add eagleJwtAccessTokenConverter

---
+++ eagle-authorization-server/src/main/java/com/tubatao/eagle/authorization/config/EagleJwtAccessTokenConverter.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright(c) tubatao Technology Co.,Ltd.
+ * All Rights Reserved.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * <p>
+ * project: eagle-authorization-server
+ * <p>
+ * Revision History:
+ * Date         Version     Name                Description
+ * 7/12/18  1.0         liuzhiheng         Creation File
+ */
+package com.tubatao.eagle.authorization.config;
+
+import com.tubatao.eagle.authorization.constants.SecurityConstants;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+
+import java.util.Map;
+
+/**
+ * Description:
+ *
+ *
+ * @author liuzhiheng
+ * @date 7/12/18 9:20 AM
+ */
+public class EagleJwtAccessTokenConverter extends JwtAccessTokenConverter {
+    @Override
+    public Map<String, ?> convertAccessToken(OAuth2AccessToken token, OAuth2Authentication authentication) {
+        Map<String, Object> representation = (Map<String, Object>) super.convertAccessToken(token, authentication);
+        representation.put("license", SecurityConstants.EAGLE_LICENSE);
+        return representation;
+    }
+
+    @Override
+    public OAuth2AccessToken extractAccessToken(String value, Map<String, ?> map) {
+        return super.extractAccessToken(value, map);
+    }
+
+    @Override
+    public OAuth2Authentication extractAuthentication(Map<String, ?> map) {
+        return super.extractAuthentication(map);
+    }
+}
