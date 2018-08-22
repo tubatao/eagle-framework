0411c2740b90433073b545dd97529f928624ed3c
liuzhiheng <liuzhiheng@tubatao.com>
2018-08-22 17:09:03 +0800

    feat: add mobileAuthenticationtToken

---
+++ eagle-authorization-server/src/main/java/com/tubatao/eagle/authorization/mobile/MobileAuthenticationToken.java
@@ -0,0 +1,88 @@
+/**
+ * Copyright(c) tubatao Technology Co.,Ltd.
+ * All Rights Reserved.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * <p>
+ * project: eagle-authorization-server
+ * <p>
+ * Revision History:
+ * Date         Version     Name                Description
+ * 7/11/18  1.0         liuzhiheng         Creation File
+ */
+package com.tubatao.eagle.authorization.mobile;
+
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.SpringSecurityCoreVersion;
+import java.util.Collection;
+
+/**
+ * Description:
+ * Mobile Authentication Token
+ * An {@link org.springframework.security.core.Authentication} implementation that is
+ * designed for simple presentation of a mobile and code.
+ * <p>
+ * The <code>principal</code> and <code>credentials</code> should be set with an
+ * <code>Object</code> that provides the respective property via its
+ * <code>Object.toString()</code> method. The simplest such <code>Object</code> to use is
+ * <code>String</code>.
+ *
+ * @author liuzhiheng
+ * @date 7/11/18 3:55 PM
+ */
+public class MobileAuthenticationToken extends AbstractAuthenticationToken{
+    private static final long serialVersionUID = SpringSecurityCoreVersion.SERIAL_VERSION_UID;
+
+    private final Object principal;
+    private Object credentials;
+
+    public MobileAuthenticationToken(Object principal, Object credentials) {
+        super((Collection)null);
+        this.principal = principal;
+        this.credentials = credentials;
+        this.setAuthenticated(false);
+    }
+
+    public MobileAuthenticationToken(Object principal, Object credentials,Collection<? extends GrantedAuthority> authorities) {
+        super(authorities);
+        this.principal = principal;
+        this.credentials = credentials;
+        super.setAuthenticated(true);
+    }
+
+    @Override
+    public Object getCredentials() {
+        return this.credentials;
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return this.principal;
+    }
+
+    @Override
+    public void setAuthenticated(boolean isAuthenticated) throws IllegalArgumentException {
+        if (isAuthenticated) {
+            throw new IllegalArgumentException(
+                    "Cannot set this token to trusted - use constructor which takes a GrantedAuthority list instead");
+        }
+        super.setAuthenticated(false);
+    }
+
+    @Override
+    public void eraseCredentials() {
+        super.eraseCredentials();
+        this.credentials = null;
+    }
+}
