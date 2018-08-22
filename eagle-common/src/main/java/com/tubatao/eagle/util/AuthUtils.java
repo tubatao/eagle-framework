3a7d760bcb477021644c09d8074706c3b3ed3aa8
liuzhiheng <liuzhiheng@tubatao.com>
2018-08-22 15:08:13 +0800

    feat: add authUtils

---
+++ eagle-common/src/main/java/com/tubatao/eagle/util/AuthUtils.java
@@ -0,0 +1,89 @@
+/**
+ * Copyright(c) tubatao Technology Co.,Ltd.
+ * All Rights Reserved.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * <p>
+ * project: eagle-framework
+ * <p>
+ * Revision History:
+ * Date         Version     Name                Description
+ * 8/1/2018  1.0         liuzhiheng         Creation File
+ */
+package com.tubatao.eagle.util;
+
+import com.tubatao.eagle.common.constant.CommonConstant;
+import com.tubatao.eagle.core.exception.CheckedException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.codec.Base64;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * Description:
+ * 认证授权相关工具类
+ *
+ * @author liuzhiheng
+ * @date 8/1/2018 2:34 PM
+ */
+@Slf4j
+public class AuthUtils {
+    private static final String BASIC_ = "Basic ";
+
+    /**
+     * 从header 请求中的clientId/clientsecect
+     *
+     * @param header header中的参数
+     * @throws CheckedException if the Basic header is not present or is not valid
+     *                          Base64
+     */
+    public static String[] extractAndDecodeHeader(String header)
+            throws IOException {
+
+        byte[] base64Token = header.substring(6).getBytes("UTF-8");
+        byte[] decoded;
+        try {
+            decoded = Base64.decode(base64Token);
+        } catch (IllegalArgumentException e) {
+            throw new CheckedException(
+                    "Failed to decode basic authentication token");
+        }
+
+        String token = new String(decoded, CommonConstant.UTF8);
+
+        int delim = token.indexOf(":");
+
+        if (delim == -1) {
+            throw new CheckedException("Invalid basic authentication token");
+        }
+        return new String[]{token.substring(0, delim), token.substring(delim + 1)};
+    }
+
+    /**
+     * *从header 请求中的clientId/clientsecect
+     *
+     * @param request
+     * @return
+     * @throws IOException
+     */
+    public static String[] extractAndDecodeHeader(HttpServletRequest request)
+            throws IOException {
+        String header = request.getHeader("Authorization");
+
+        if (header == null || !header.startsWith(BASIC_)) {
+            throw new CheckedException("请求头中client信息为空");
+        }
+
+        return extractAndDecodeHeader(header);
+    }
+}
