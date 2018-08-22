e46d5e74055530802370ef67a2b2b45216d4bb4a
liuzhiheng <liuzhiheng@tubatao.com>
2018-08-22 15:45:43 +0800

    feat: add filterIgnorePropertiesConfig

---
+++ eagle-common/src/main/java/com/tubatao/eagle/core/config/FilterIgnorePropertiesConfig.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright(c) tubatao Technology Co.,Ltd.
+ * All Rights Reserved.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * <p>
+ * project: eagle-authorization-server
+ * <p>
+ * Revision History:
+ * Date         Version     Name                Description
+ * 7/12/18  1.0         liuzhiheng         Creation File
+ */
+package com.tubatao.eagle.core.config;
+
+import lombok.Data;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Configuration;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Description:
+ *
+ *
+ * @author liuzhiheng
+ * @date 7/12/18 9:30 AM
+ */
+@Data
+@Configuration
+@RefreshScope
+@ConditionalOnExpression("!'${ignore}'.isEmpty()")
+@ConfigurationProperties(prefix = "ignore")
+public class FilterIgnorePropertiesConfig {
+    private List<String> urls = new ArrayList<>();
+
+    private List<String> clients = new ArrayList<>();
+}
