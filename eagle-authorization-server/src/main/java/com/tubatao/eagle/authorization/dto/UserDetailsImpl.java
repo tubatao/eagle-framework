0893060cb2c3147b63cc65fc57d6c62be28097bc
liuzhiheng <liuzhiheng@tubatao.com>
2018-08-22 17:11:33 +0800

    feat: add userDetailsImpl

---
+++ eagle-authorization-server/src/main/java/com/tubatao/eagle/authorization/dto/UserDetailsImpl.java
@@ -0,0 +1,129 @@
+/**
+ * Copyright(c) tubatao Technology Co.,Ltd.
+ * All Rights Reserved.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * <p>
+ * project: eagle-authorization-server
+ * <p>
+ * Revision History:
+ * Date         Version     Name                Description
+ * 7/11/18  1.0         liuzhiheng         Creation File
+ */
+package com.tubatao.eagle.authorization.dto;
+
+
+import com.tubatao.eagle.authorization.constants.CommonConstant;
+import com.tubatao.eagle.authorization.constants.SecurityConstants;
+import com.tubatao.eagle.authorization.vo.SysRole;
+import com.tubatao.eagle.authorization.vo.UserVO;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Description:
+ *
+ *
+ * @author liuzhiheng
+ * @date 7/11/18 4:30 PM
+ */
+public class UserDetailsImpl implements UserDetails {
+    private static final long serialVersionUID = 1L;
+    private Integer userId;
+    private String username;
+    private String password;
+    private String status;
+    private List<SysRole> roleList;
+
+    public UserDetailsImpl(UserVO userVo) {
+        this.userId = userVo.getUserId();
+        this.username = userVo.getUsername();
+        this.password = userVo.getPassword();
+        this.status = userVo.getDelFlag();
+        roleList = userVo.getRoleList();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorityList = new ArrayList<>();
+        for (SysRole role : roleList) {
+            authorityList.add(new SimpleGrantedAuthority(role.getRoleCode()));
+        }
+        authorityList.add(new SimpleGrantedAuthority(SecurityConstants.BASE_ROLE));
+        return authorityList;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return StringUtils.equals(CommonConstant.STATUS_LOCK, status) ? false : true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return StringUtils.equals(CommonConstant.STATUS_NORMAL, status) ? true : false;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<SysRole> getRoleList() {
+        return roleList;
+    }
+
+    public void setRoleList(List<SysRole> roleList) {
+        this.roleList = roleList;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
