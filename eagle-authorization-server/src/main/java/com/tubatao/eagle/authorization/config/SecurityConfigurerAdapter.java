f3e0ab65b887aede25163a8b8e6639f58d03d3e3
liuzhiheng <liuzhiheng@tubatao.com>
2018-08-22 17:13:41 +0800

    feat: add securityConfigurerAdapter

---
+++ eagle-authorization-server/src/main/java/com/tubatao/eagle/authorization/config/SecurityConfigurerAdapter.java
@@ -0,0 +1,74 @@
+/**
+ * Copyright(c) tubatao Technology Co.,Ltd.
+ * All Rights Reserved.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * <p>
+ * project: eagle-authorization-server
+ * <p>
+ * Revision History:
+ * Date         Version     Name                Description
+ * 7/12/18  1.0         liuzhiheng         Creation File
+ */
+package com.tubatao.eagle.authorization.config;
+
+import com.tubatao.eagle.authorization.config.properties.FilterIgnorePropertiesConfig;
+import com.tubatao.eagle.authorization.mobile.MobileSecurityConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+
+/**
+ * Description:
+ *
+ *
+ * @author liuzhiheng
+ * @date 7/12/18 9:27 AM
+ */
+@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER - 1)
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private FilterIgnorePropertiesConfig filterIgnorePropertiesConfig;
+    @Autowired
+    private MobileSecurityConfig mobileSecurityConfigurer;
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        ExpressionUrlAuthorizationConfigurer<HttpSecurity>.ExpressionInterceptUrlRegistry registry =
+                http.formLogin().loginPage("/authentication/require")
+                        .loginProcessingUrl("/authentication/form")
+                        .and()
+                        .authorizeRequests();
+        filterIgnorePropertiesConfig.getUrls().forEach(url -> registry.antMatchers(url).permitAll());
+        registry.anyRequest().authenticated()
+                .and()
+                .csrf().disable();
+        http.apply(mobileSecurityConfigurer);
+    }
+
+    @Bean(name = BeanIds.AUTHENTICATION_MANAGER)
+    @Override
+    public AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+}
