f5334b3af6c97dd7fb500839248f0f7693a38afb
liuzhiheng <liuzhiheng@tubatao.com>
2018-08-22 19:20:48 +0800

    feat: add springContextUtil

---
+++ eagle-common/src/main/java/com/tubatao/eagle/util/SpringContextUtil.java
@@ -0,0 +1,79 @@
+/**
+ * Copyright(c) tubatao Technology Co.,Ltd.
+ * All Rights Reserved.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * <p>
+ * project: eagle-framework
+ * <p>
+ * Revision History:
+ * Date         Version     Name                Description
+ * 8/22/2018  1.0         liuzhiheng         Creation File
+ */
+package com.tubatao.eagle.util;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+/**
+ * Description:
+ * 这个类是为了解决在普通类调用service的问题
+ *
+ * @author liuzhiheng
+ * @date 7/12/18 5:56 PM
+ */
+public class SpringContextUtil implements ApplicationContextAware {
+    private static ApplicationContext applicationContext; // Spring应用上下文
+
+    // 下面的这个方法上加了@Override注解，原因是继承ApplicationContextAware接口是必须实现的方法
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext)
+            throws BeansException {
+        SpringContextUtil.applicationContext = applicationContext;
+    }
+
+    public static ApplicationContext getApplicationContext() {
+        return applicationContext;
+    }
+
+    public static Object getBean(String name) throws BeansException {
+        return applicationContext.getBean(name);
+    }
+
+    public static Object getBean(String name, Class requiredType)
+            throws BeansException {
+
+        return applicationContext.getBean(name, requiredType);
+    }
+
+    public static boolean containsBean(String name) {
+        return applicationContext.containsBean(name);
+    }
+
+    public static boolean isSingleton(String name)
+            throws NoSuchBeanDefinitionException {
+        return applicationContext.isSingleton(name);
+    }
+
+    public static Class getType(String name)
+            throws NoSuchBeanDefinitionException {
+        return applicationContext.getType(name);
+    }
+
+    public static String[] getAliases(String name)
+            throws NoSuchBeanDefinitionException {
+        return applicationContext.getAliases(name);
+    }
+}
