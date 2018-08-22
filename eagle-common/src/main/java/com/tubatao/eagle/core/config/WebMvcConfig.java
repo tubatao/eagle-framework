7c08a7a47339225c6af6eb6f02b5b3d066c19408
liuzhiheng <liuzhiheng@tubatao.com>
2018-08-22 15:49:23 +0800

    feat: add webMvcConfig

---
+++ eagle-common/src/main/java/com/tubatao/eagle/core/config/WebMvcConfig.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright(c) tubatao Technology Co.,Ltd.
+ * All Rights Reserved.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * <p>
+ * project: eagle-framework
+ * <p>
+ * Revision History:
+ * Date         Version     Name                Description
+ * 8/1/2018  1.0         liuzhiheng         Creation File
+ */
+package com.tubatao.eagle.core.config;
+
+import com.tubatao.eagle.common.resolver.TokenArgumentResolver;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import java.util.List;
+
+/**
+ * Description:
+ * mvc配置
+ *
+ * @author liuzhiheng
+ * @date 8/1/2018 12:43 PM
+ */
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(new TokenArgumentResolver());
+    }
+}
