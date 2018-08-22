2498e7745bf9a1bf8ac54a5912e0c794bb7822c5
liuzhiheng <liuzhiheng@tubatao.com>
2018-08-22 17:08:41 +0800

    feat: add mobileAuthenticationProvider

---
+++ eagle-authorization-server/src/main/java/com/tubatao/eagle/authorization/mobile/MobileAuthenticationProvider.java
@@ -0,0 +1,76 @@
+/**
+ * Copyright(c) tubatao Technology Co.,Ltd.
+ * All Rights Reserved.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * <p>
+ * project: eagle-authorization-server
+ * <p>
+ * Revision History:
+ * Date         Version     Name                Description
+ * 7/11/18  1.0         liuzhiheng         Creation File
+ */
+package com.tubatao.eagle.authorization.mobile;
+
+
+import com.tubatao.eagle.authorization.dto.UserDetailsImpl;
+import com.tubatao.eagle.authorization.feign.UserService;
+import com.tubatao.eagle.authorization.vo.UserVO;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+/**
+ * Description:
+ * 手机号登录校验逻辑
+ *
+ * @author liuzhiheng
+ * @date 7/11/18 3:55 PM
+ */
+public class MobileAuthenticationProvider implements AuthenticationProvider{
+    private UserService userService;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        MobileAuthenticationToken mobileAuthenticationToken = (MobileAuthenticationToken) authentication;
+        UserVO userVo = userService.findUserByMobile((String) mobileAuthenticationToken.getPrincipal());
+
+        if (userVo == null) {
+            throw new UsernameNotFoundException("手机号不存在:" + mobileAuthenticationToken.getPrincipal());
+        }
+
+        UserDetailsImpl userDetails = buildUserDeatils(userVo);
+
+        MobileAuthenticationToken authenticationToken = new MobileAuthenticationToken(userDetails, userDetails.getAuthorities());
+        authenticationToken.setDetails(mobileAuthenticationToken.getDetails());
+        return authenticationToken;
+    }
+
+    private UserDetailsImpl buildUserDeatils(UserVO userVo) {
+        return new UserDetailsImpl(userVo);
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return MobileAuthenticationToken.class.isAssignableFrom(authentication);
+    }
+
+    public UserService getUserService() {
+        return userService;
+    }
+
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+}
