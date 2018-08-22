aec7b7ffe919bc63ac3bf01c7c073e7d3611ae29
liuzhiheng <liuzhiheng@tubatao.com>
2018-08-22 15:09:25 +0800

    feat: add query for mybatis

---
+++ eagle-common/src/main/java/com/tubatao/eagle/util/Query.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright(c) tubatao Technology Co.,Ltd.
+ * All Rights Reserved.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * <p>
+ * project: eagle-framework
+ * <p>
+ * Revision History:
+ * Date         Version     Name                Description
+ * 8/1/2018  1.0         liuzhiheng         Creation File
+ */
+package com.tubatao.eagle.util;
+
+import com.baomidou.mybatisplus.plugins.Page;
+import org.apache.commons.lang3.StringUtils;
+import java.util.Map;
+
+/**
+ * Description:
+ *
+ * @author liuzhiheng
+ * @date 8/1/2018 2:35 PM
+ */
+public class Query<T> extends Page<T> {
+    private static final String PAGE = "page";
+    private static final String LIMIT = "limit";
+    private static final String ORDER_BY_FIELD = "orderByField";
+    private static final String IS_ASC = "isAsc";
+
+    public Query(Map<String, Object> params) {
+        super(Integer.parseInt(params.getOrDefault(PAGE, 1).toString())
+                , Integer.parseInt(params.getOrDefault(LIMIT, 10).toString()));
+
+        String orderByField = params.getOrDefault(ORDER_BY_FIELD, "").toString();
+        if (StringUtils.isNotEmpty(orderByField)) {
+            this.setOrderByField(orderByField);
+        }
+
+        Boolean isAsc = Boolean.parseBoolean(params.getOrDefault(IS_ASC, Boolean.TRUE).toString());
+        this.setAsc(isAsc);
+
+        params.remove(PAGE);
+        params.remove(LIMIT);
+        params.remove(ORDER_BY_FIELD);
+        params.remove(IS_ASC);
+        this.setCondition(params);
+    }
+}
