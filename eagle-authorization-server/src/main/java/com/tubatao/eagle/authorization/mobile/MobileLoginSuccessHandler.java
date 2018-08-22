b9eeb7543cec840f77a1607937d52b5970eb5d46
liuzhiheng <liuzhiheng@tubatao.com>
2018-08-22 17:09:27 +0800

    feat: add mobileLoginSuccessHandler

---
+++ eagle-authorization-server/src/main/java/com/tubatao/eagle/authorization/mobile/MobileLoginSuccessHandler.java
@@ -0,0 +1,100 @@
+/**
+ * Copyright(c) tubatao Technology Co.,Ltd.
+ * All Rights Reserved.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * <p>
+ * project: eagle-authorization-server
+ * <p>
+ * Revision History:
+ * Date         Version     Name                Description
+ * 7/11/18  1.0         liuzhiheng         Creation File
+ */
+package com.tubatao.eagle.authorization.mobile;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tubatao.eagle.authorization.constants.CommonConstant;
+import com.tubatao.eagle.authorization.utils.AuthUtils;
+import com.xiaoleilu.hutool.map.MapUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.common.exceptions.UnapprovedClientAuthenticationException;
+import org.springframework.security.oauth2.provider.*;
+import org.springframework.security.oauth2.provider.token.AuthorizationServerTokenServices;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Description:
+ * 手机号登录成功，返回oauth token
+ *
+ * @author liuzhiheng
+ * @date 7/11/18 3:55 PM
+ */
+@Slf4j
+@Component
+public class MobileLoginSuccessHandler implements AuthenticationSuccessHandler{
+    public static final String BASIC_ = "Basic ";
+    @Autowired
+    private ObjectMapper objectMapper;
+    @Autowired
+    private ClientDetailsService clientDetailsService;
+    @Autowired
+    private AuthorizationServerTokenServices authorizationServerTokenServices;
+
+    /**
+     * Called when a user has been successfully authenticated.
+     * 调用spring security oauth API 生成 oAuth2AccessToken
+     *
+     * @param request        the request which caused the successful authentication
+     * @param response       the response
+     * @param authentication the <tt>Authentication</tt> object which was created during
+     */
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+        String header = request.getHeader("Authorization");
+
+        if (header == null || !header.startsWith(BASIC_)) {
+            throw new UnapprovedClientAuthenticationException("The client information in the request header is empty.");
+        }
+
+        try {
+            String[] tokens = AuthUtils.extractAndDecodeHeader(header);
+            assert tokens.length == 2;
+            String clientId = tokens[0];
+
+            ClientDetails clientDetails = clientDetailsService.loadClientByClientId(clientId);
+            TokenRequest tokenRequest = new TokenRequest(MapUtil.newHashMap(), clientId, clientDetails.getScope(), "mobile");
+            OAuth2Request oAuth2Request = tokenRequest.createOAuth2Request(clientDetails);
+
+            OAuth2Authentication oAuth2Authentication = new OAuth2Authentication(oAuth2Request, authentication);
+            OAuth2AccessToken oAuth2AccessToken = authorizationServerTokenServices.createAccessToken(oAuth2Authentication);
+            log.info("Get the token successfully ：{}", oAuth2AccessToken.getValue());
+
+            response.setCharacterEncoding(CommonConstant.UTF8);
+            response.setContentType(CommonConstant.CONTENT_TYPE);
+            PrintWriter printWriter = response.getWriter();
+            printWriter.append(objectMapper.writeValueAsString(oAuth2AccessToken));
+        } catch (IOException e) {
+            throw new BadCredentialsException("Failed to decode basic authentication token");
+        }
+    }
+}
