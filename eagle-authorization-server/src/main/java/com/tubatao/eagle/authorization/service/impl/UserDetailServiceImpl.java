73682cf58795d9165080887756d9f3b290f6de8a
liuzhiheng <liuzhiheng@tubatao.com>
2018-08-22 17:07:38 +0800

    feat: add userDetailServiceImpl

---
+++ eagle-authorization-server/src/main/java/com/tubatao/eagle/authorization/service/impl/UserDetailServiceImpl.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright(c) tubatao Technology Co.,Ltd.
+ * All Rights Reserved.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * <p>
+ * project: eagle-authorization-server
+ * <p>
+ * Revision History:
+ * Date         Version     Name                Description
+ * 7/11/18  1.0         liuzhiheng         Creation File
+ */
+package com.tubatao.eagle.authorization.service.impl;
+
+
+import com.tubatao.eagle.authorization.dto.UserDetailsImpl;
+import com.tubatao.eagle.authorization.feign.UserService;
+import com.tubatao.eagle.authorization.vo.UserVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+/**
+ * Description:
+ *
+ *
+ * @author liuzhiheng
+ * @date 7/11/18 5:21 PM
+ */
+@Service
+public class UserDetailServiceImpl implements UserDetailsService{
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public UserDetailsImpl loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserVO userVo = userService.findUserByUsername(username);
+        return new UserDetailsImpl(userVo);
+    }
+}
