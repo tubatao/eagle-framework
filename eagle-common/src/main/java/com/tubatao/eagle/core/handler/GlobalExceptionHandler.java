def81cd09e3c25a787a031ef953fd7aa5949178e
liuzhiheng <liuzhiheng@tubatao.com>
2018-08-22 15:05:18 +0800

    feat: add globalExceptionHandler

---
+++ eagle-common/src/main/java/com/tubatao/eagle/core/handler/GlobalExceptionHandler.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright(c) tubatao Technology Co.,Ltd.
+ * All Rights Reserved.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * <p>
+ * project: eagle-framework
+ * <p>
+ * Revision History:
+ * Date         Version     Name                Description
+ * 8/1/2018  1.0         liuzhiheng         Creation File
+ */
+package com.tubatao.eagle.core.handler;
+
+import com.tubatao.eagle.common.bean.R;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * Description:
+ * 全局的的异常拦截器
+ *
+ *
+ * @author liuzhiheng
+ * @date 8/1/2018 12:44 PM
+ */
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    /**
+     * 全局异常.
+     *
+     * @param e the e
+     * @return R
+     */
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseBody
+    public R exception(Exception e) {
+        log.info("保存全局异常信息 ex={}", e.getMessage(), e);
+        return new R<>(e);
+    }
+}
