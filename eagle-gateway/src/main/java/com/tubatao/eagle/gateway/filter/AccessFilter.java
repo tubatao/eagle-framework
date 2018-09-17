21ffeff3fdaaf761f16811e5f4d625298a2dde90
liuzhiheng <liuzhiheng@tubatao.com>
2018-09-17 23:44:44 +0800

    feat: add accessFilter

--- eagle-gateway/src/main/java/com/tubatao/eagle/gateway/filter/AccessFilter.java
+++ eagle-gateway/src/main/java/com/tubatao/eagle/gateway/filter/AccessFilter.java
@@ -22,11 +22,52 @@
  */
 package com.tubatao.eagle.gateway.filter;
 
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.tubatao.eagle.common.constant.SecurityConstants;
+import com.xiaoleilu.hutool.collection.CollectionUtil;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.FORM_BODY_WRAPPER_FILTER_ORDER;
+
 /**
  * Description:
+ * 在RateLimitPreFilter 之前执行，不然又空指针问题
  *
  * @author liuzhiheng
  * @date 8/29/2018 4:37 PM
  */
-public class AccessFilter {
+@Component
+public class AccessFilter extends ZuulFilter {
+
+    @Override
+    public String filterType() {
+        return FilterConstants.PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return FORM_BODY_WRAPPER_FILTER_ORDER - 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        ctx.set("startTime", System.currentTimeMillis());
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null) {
+            RequestContext requestContext = RequestContext.getCurrentContext();
+            requestContext.addZuulRequestHeader(SecurityConstants.USER_HEADER, authentication.getName());
+            requestContext.addZuulRequestHeader(SecurityConstants.ROLE_HEADER,  CollectionUtil.join(authentication.getAuthorities(),","));
+        }
+        return null;
+    }
 }
