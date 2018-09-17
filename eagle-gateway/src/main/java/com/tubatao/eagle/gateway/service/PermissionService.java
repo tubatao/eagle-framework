481cd18c0ae859148c8d9f7e73a8bf2902cae81f
liuzhiheng <liuzhiheng@tubatao.com>
2018-09-17 23:42:51 +0800

    feat: add permissionService

--- eagle-gateway/src/main/java/com/tubatao/eagle/gateway/service/PermissionService.java
+++ eagle-gateway/src/main/java/com/tubatao/eagle/gateway/service/PermissionService.java
@@ -22,11 +22,23 @@
  */
 package com.tubatao.eagle.gateway.service;
 
+import org.springframework.security.core.Authentication;
+
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * Description:
  *
  * @author liuzhiheng
  * @date 8/29/2018 3:58 PM
  */
-public class PermissionService {
+public interface PermissionService {
+    /**
+     * 判断请求是否有权限
+     *
+     * @param request        HttpServletRequest
+     * @param authentication 认证信息
+     * @return 是否有权限
+     */
+    boolean hasPermission(HttpServletRequest request, Authentication authentication);
 }
